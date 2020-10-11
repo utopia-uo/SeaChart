608cdb348e665e08ce41a54d1ddfefe9d23a596f
Sébastien Santoro <dereckson@espace-win.org>
2020-10-11 07:19:49 +0200

    Clean namespaces

--- Helpers classes/SerializableDictionary.cs
+++ Helpers classes/SerializableDictionary.cs
@@ -6,8 +6,7 @@
  * The key and value types must be themselves serializable.
  */
 
-using System;
-using System.Collections.Generic;
+using System.Xml.Schema;
 using System.Xml.Serialization;
 using System.Xml;
 
